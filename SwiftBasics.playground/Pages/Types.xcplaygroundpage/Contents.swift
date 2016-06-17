5bfef35d1f98d4a4d3145101bfbd772096e9703e
James McMillan <james_mcmillan@icloud.com>
2016-06-16 21:10:08 -0700

    Course Complete
    Swift Playground Notes from Swift 2.0 Basics course

---
+++ SwiftBasics.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,66 @@
+//: [Previous](@previous)
+
+//////////////////////////////////////////
+////////////// Strings //////////////////
+/////////////////////////////////////////
+
+
+// Concatenation
+
+let country = "United States of America"
+let state = "California"
+let city = "Santa Ana"
+
+let concatenatedAddress = country + ", " + state + ", " + city
+
+// String Interpolation
+
+let interpolatedAddress = "\(country), \(state), \(city)"
+
+let interpolatedStreetAddress = "\(222) + \(state)"
+
+//////////////////////////////////////////
+////////////// Integers //////////////////
+/////////////////////////////////////////
+
+let favoriteProgrammingLanguage = "Swift"
+let year = 2014 // Int
+
+
+//////////////////////////////////////////
+////// Floating Point Numbers ///////////
+/////////////////////////////////////////
+
+var version = 2.0 // Double
+
+
+//////////////////////////////////////////
+///////////// Boolean ///////////////////
+/////////////////////////////////////////
+
+let isAwesome = true // Bool
+
+
+//////////////////////////////////////////
+///////////// Type Saftey ///////////////
+/////////////////////////////////////////
+
+var someString = ""
+// someString = 12.0
+
+
+let bestPlayer: String = "Michael Jordan"
+let averagePointsPerGame: Double = 30.1
+let yearOfRetirement: Int = 2003
+let hallOfFame: Bool = true
+
+let firstValue: Int = 123
+let secondValue: Int = 456
+
+let product: Int = firstValue * secondValue
+
+let output: String = "The product of \(firstValue) times \(secondValue) is \(product)."
+
+
+//: [Next](@next)
+
